8718d2f43a1474d5bba933cdc1aace6edf15d3c4
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-10 09:06:35 +0100

    ArrowFunctionsAndThis

--- ArrowFunctionsAndThis.js
+++ ArrowFunctionsAndThis.js
@@ -1,8 +1,23 @@
 const robot = {
   energyLevel: 100,
+  checkEnergy (){
+    console.log(`Energy is currently at ${this.energyLevel}%.`);
+  },
+};
+
+robot.checkEnergy();
+
+console.log("############ consoles logs ##########");
+console.log(robot.energyLevel);
+console.log(robot.checkEnergy);
+
+/* original code => suppression of the arrow and the colon.
+const robot = {
+  energyLevel: 100,
   checkEnergy: () => {
     console.log(`Energy is currently at ${this.energyLevel}%.`)
   }
 }
 
 robot.checkEnergy();
+*/
--- advancedObjects.js
+++ advancedObjects.js
@@ -18,7 +18,28 @@ console.log(robot.provideInfo())
 
 // #################### Arrow Functions and this ####################
 /*
+const robot = {
+  energyLevel: 100,
+  checkEnergy (){
+    console.log(`Energy is currently at ${this.energyLevel}%.`);
+  },
+};
 
+robot.checkEnergy();
+
+console.log("############ consoles logs ##########");
+console.log(robot.energyLevel);
+console.log(robot.checkEnergy);
+
+/* original code => suppression of the arrow and the colon.
+const robot = {
+  energyLevel: 100,
+  checkEnergy: () => {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
 */
 
 // #################### First exemple shown ####################
