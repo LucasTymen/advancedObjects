dc1ff1d382b70e593941147559250abd84c2d0dc
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-13 12:08:18 +0100

    FactoryFunctions

--- FactoryFunctions.js
+++ FactoryFunctions.js
@@ -1,8 +1,22 @@
+const robotFactory = (model, mobile, beep) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep(){
+      console.log('Beep Boop')
+    }
+  };
+};
+const tinCan = robotFactory('P-500',true)
+console.log(tinCan.beep())
+
+// exemples
 const monsterFactory = (name, age, energySource, catchPhrase) => {
   return {
     name: name,
     age: age,
     energySource: energySource,
+
     scare() {
       console.log(catchPhrase);
     }
