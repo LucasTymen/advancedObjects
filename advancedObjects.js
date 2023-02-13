399e2a02c68c5ead64e1006bd63e897104314284
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-13 15:24:56 +0100

    Built-inObjectMethods

--- advancedObjects.js
+++ advancedObjects.js
@@ -245,6 +245,23 @@ const chargingStation = {
   }
 }
 
+// ################### Built-in Object Methods ###################
+/*
+In the previous exercises we’ve been creating instances of objects that have
+their own methods. But, we can also take advantage of built-in methods for
+Objects!
+
+For example, we have access to object instance methods like: .hasOwnProperty(),
+.valueOf(), and many more! Practice your documentation reading skills and check
+out: MDN’s object instance documentation.
+
+There are also useful Object class methods such as Object.assign(),
+Object.entries(), and Object.keys() just to name a few. For a comprehensive
+list, browse: MDN’s object instance documentation.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object#Methods
+*/
+
+
 // ################# A propos des objets ####################
 /*
 Global object
@@ -311,3 +328,47 @@ param => {
 }
 
  */
+
+// ################ Review ################
+/*
+Let’s review the concepts covered in this lesson:
+
+    The object that a method belongs to is called the calling object.
+    The this keyword refers to the calling object and can be used to access
+    properties of the calling object.
+    Methods do not automatically have access to other internal properties of the
+    calling object.
+    The value of this depends on where the this is being accessed from.
+    We cannot use arrow functions as methods if we want to access other internal
+    properties.
+    JavaScript objects do not have built-in privacy, rather there are
+    conventions to follow to notify other developers about the intent of the
+    code.
+    The usage of an underscore before a property name means that the original
+    developer did not intend for that property to be directly changed.
+    Setters and getter methods allow for more detailed ways of accessing and
+    assigning properties.
+    Factory functions allow us to create object instances quickly and
+    repeatedly.
+    There are different ways to use object destructuring: one way is the
+    property value shorthand and another is destructured assignment.
+    As with any concept, it is a good skill to learn how to use the
+    documentation with objects!
+
+You’re ready to start leveraging more elegant code for creating and accessing
+objects in your code!
+Instructions
+
+If you want to challenge yourself:
+
+    Find the value of this in a function inside of a method.
+    Learn the outcome of using a property that has the exact same name as a
+    setter/getter method.
+    Create a new factory function that can create object instances of your c
+    hoice.
+    Read documentation on other destructuring techniques and apply it to your
+    code.
+    Try out other built-in object methods and learn what they do.
+
+
+*/
